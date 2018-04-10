8ee47fc1a6bf82a26f530a6862025fa301bbf746
Jakub Jabłoński <kuba.jablonski.zg@gmail.com>
2018-04-10 09:03:25 +0200

    style settings control

--- src/components/SettingsControl.vue
+++ src/components/SettingsControl.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="settings-control">
-    <button @click="$emit('change', value - 60)" class="settings-control__button">&lt;</button>
+    <button @click="$emit('change', value - 60)" :disabled="value <= min" class="settings-control__btn">
+      <svg class="settings-control__icon">
+        <use xlink:href="/static/sprite.svg#icon-chevron-thin-left"></use>
+      </svg>
+    </button>
     <span class="settings-control__display">{{ value | secToMin }}</span>
-    <button @click="$emit('change', value + 60)" class="settings-control__button">&gt;</button>
+    <button @click="$emit('change', value + 60)" :disabled="value >= max" class="settings-control__btn">
+      <svg class="settings-control__icon">
+        <use xlink:href="/static/sprite.svg#icon-chevron-thin-right"></use>
+      </svg>
+    </button>
   </div>
 </template>
 
@@ -16,3 +24,45 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.settings-control {
+  display: flex;
+  align-items: center;
+
+  &__display {
+    display: block;
+    color: #fff;
+    font-size: 8rem;
+    width: 10rem;
+    text-align: center;
+  }
+
+  &__btn {
+    background-color: transparent;
+    border: none;
+    outline: none;
+    height: 8rem;
+    width: 8rem;
+
+    &:hover,
+    &:active {
+      background-color: rgba(#fff, .1);
+    }
+  }
+
+  &__btn:disabled &__icon {
+    fill: rgba(#000, .1);
+  }
+
+  &__btn:disabled:hover {
+    background-color: transparent;
+  }
+
+  &__icon {
+    width: 5rem;
+    height: 5rem;
+    fill: #fff;
+  }
+}
+</style>
